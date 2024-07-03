e7b38cd2b63a54da429f4b1abc07e89cac29245f
Aditi <107246395+aditipandey852@users.noreply.github.com>
2024-07-03 14:07:33 +0530

    Add files via upload

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Fast food Delivery</h2>
+        <p>Order your favorite food from the best restaurants in your city</p>
+        <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
